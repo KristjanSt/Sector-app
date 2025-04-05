9bfd994b7e26eb62750e4da3318052008bd0fab5
Kristjan Stüff <krstuf@ttu.ee>
2025-04-05 16:07:38 +0300

    Initial commit for application backend and frontend

---
+++ backend/sector-app/src/test/java/com/stuff/sector_app/SectorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stuff.sector_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SectorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
